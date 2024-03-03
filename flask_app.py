4484e93b55ead6e94f76147815d99eb4146136c3
Joan Palacios Caballero <joanpalacioscaballero@gmail.com>
2024-03-03 17:24:30 +0100

    Refactor

--- flask_app.py
+++ flask_app.py
@@ -1,40 +1,35 @@
-from flask import Flask, render_template, request, jsonify
+import datetime
 import random
+
+from flask import Flask, jsonify, render_template, request
+
 from philosophical_phrases import philosophical_phrases
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/get_phrase')
+# Function to log IP addresses to a text file
+def log_ip(ip_address):
+    with open("ip_log.txt", "a") as file:
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"{timestamp} - {ip_address}\n")
+
+
+@app.route("/")
+def index():
+    ip_address = request.remote_addr
+    log_ip(ip_address)
+    return render_template("index.html")
+
+
+@app.route("/get_phrase")
 def get_phrase():
     # Get a random philosophical phrase
     random_entry = random.choice(philosophical_phrases)
 
     # Return the phrase as JSON
-    return jsonify(phrase=random_entry['phrase'], author=random_entry['author'])
+    return jsonify(phrase=random_entry["phrase"], author=random_entry["author"])
 
-@app.route('/get_ip')
-def get_ip():
-    # Function to get the user's IP address
-    def get_real_ip():
-        if request.headers.getlist("X-Forwarded-For"):
-            ip = request.headers.getlist("X-Forwarded-For")[0]
-        else:
-            ip = request.remote_addr
-        return ip
 
-    # Get the public IP address
-    user_ip = get_real_ip()
-
-    # Append the IP to a file
-    with open('user_ips.txt', 'a') as file:
-        file.write(user_ip + '\n')
-
-    # Return the IP as JSON
-    return jsonify(ip=user_ip)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)

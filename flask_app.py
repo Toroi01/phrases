1e1aadbe443f228d492ce6b6402ddc928d35f35b
Joan Palacios Caballero <joanpalacioscaballero@gmail.com>
2024-03-03 17:55:40 +0100

    Remove IP stuff

--- flask_app.py
+++ flask_app.py
@@ -8,17 +8,8 @@ from philosophical_phrases import philosophical_phrases
 app = Flask(__name__)
 
 
-# Function to log IP addresses to a text file
-def log_ip(ip_address):
-    with open("ip_log.txt", "a") as file:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"{timestamp} - {ip_address}\n")
-
-
 @app.route("/")
 def index():
-    ip_address = request.remote_addr
-    log_ip(ip_address)
     return render_template("index.html")
 
 
